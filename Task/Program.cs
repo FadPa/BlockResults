f7bf5ebc1c0fd1715284af025e29cac6c58e4c7c
Pavel <fadeevpavel181@gmail.com>
2023-01-26 17:01:35 +0300

    Task

---
+++ Task/Program.cs
@@ -0,0 +1,28 @@
+﻿void WordSearch(string[] array1, string[] array2)
+{
+    for (int i = 0, b = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+        array2[b] = array1[i];
+        b++;
+        }
+        
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите массив строк через пробел");
+string str = Console.ReadLine();
+string[] array1 = str.Split(' ');
+string[] array2 = new string[array1.Length];
+
+WordSearch(array1, array2);
+PrintArray(array2);
